81fcf5c9eafdcc775bd69a69cbb4ac6b7d67bf40
XiangyuYan-1 <1569113093yxy@gmail.com>
2026-06-02 10:58:19 +0200

    Delete tp01 directory

--- tp01/simple-api/src/main/java/fr/takima/training/simpleapi/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.takima.training.simpleapi.repository;
-
-import fr.takima.training.simpleapi.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- tp01/simple-api/src/main/java/fr/takima/training/simpleapi/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.takima.training.simpleapi.repository;
-
-import fr.takima.training.simpleapi.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findByDepartmentName(String name);
-}
