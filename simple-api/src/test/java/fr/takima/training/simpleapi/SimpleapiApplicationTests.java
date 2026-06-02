613fe0cc63c17d019cf47124203a1a6c1555d3b5
Xiangyu Yan <xiangyu.yan@epfdu.fr>
2026-06-02 14:42:41 +0200

    Add coverage support

--- simple-api/src/test/java/fr/takima/training/simpleapi/SimpleapiApplicationTests.java
+++ simple-api/src/test/java/fr/takima/training/simpleapi/SimpleapiApplicationTests.java
@@ -1,15 +1,36 @@
 package fr.takima.training.simpleapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class SimpleapiApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void shouldReturnDepartments() throws Exception {
+        mockMvc.perform(get("/departments"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnStudents() throws Exception {
+        mockMvc.perform(get("/students"))
+                .andExpect(status().isOk());
+    }
 }
