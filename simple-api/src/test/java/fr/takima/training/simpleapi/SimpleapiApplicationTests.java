e52939b280558589023f547f1b658f624ec5e5e9
Xiangyu Yan <xiangyu.yan@epfdu.fr>
2026-06-02 14:26:53 +0200

    Split CI Sonar and Docker workflows

--- simple-api/src/test/java/fr/takima/training/simpleapi/SimpleapiApplicationTests.java
+++ simple-api/src/test/java/fr/takima/training/simpleapi/SimpleapiApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class SimpleapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
